a4826d3e77868f7983f2c2b946d96e1089337891
Eric Curti <ericfacultad@gmail.com>
2024-09-27 15:52:21 -0600

    js constants and state

---
+++ js/index.js
@@ -0,0 +1,12 @@
+//Constants
+const CART_ICON_SELECTOR = '#cart-icon i';
+const CART_MODAL_ID = 'cart-modal';
+const CART_ITEMS_ID = 'cart-items';
+const CART_TOTAL_ID = 'cart-total';
+const CHECKOUT_BTN_ID = 'checkout-btn';
+const ADD_TO_CART_CLASS = 'add-to-cart';
+
+//State
+let cart = [];
+let products = [];
+let cartItemCount = 0;
